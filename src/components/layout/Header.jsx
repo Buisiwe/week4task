bfe4e3092ca6a348f8a047f3d1c21f5b7dee3bd1
Matheej <Mpho.Matee@vodacom.co.za>
2021-06-08 15:21:47 +0200

    Week5

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>Buisiwe's TodoList Site</h1>
+        </header>
+    )
+}
+
+const headerStyle = {
+    background: '#7e57c2',
+    color: '#ffffff',
+    textAlign: 'center',
+    padding: '10px'
+}
+
+
+export default Header;
